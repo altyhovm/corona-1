a55c01be4aaffd22808623f66c3eff32dc37bae3
Tugrul Bıyık <44803788+tugib@users.noreply.github.com>
2020-04-04 03:12:19 +0300

    update

--- index.php
+++ index.php
@@ -2,13 +2,10 @@
 require_once("config.php");
 include("select.php");
 foreach ($values->data as $key => $value) {
-  ?>
-
-
-    <div class="ule">
-      <a href="<?=$url?>location/<?php echo replace($value->location); ?>">
-        <?php echo $value->location; ?>
-      </a>
-
-    </div>
+?>
+  <div class="ule">
+    <a href="<?=$url?>location/<?php echo replace($value->location); ?>">
+      <?php echo $value->location; ?>
+    </a>
+  </div>
 <?php }  ?>
---
+++ readme.txt
@@ -0,0 +1,5 @@
+Covid-19 api service
+
+USAGE:
+Download package
+Change "url" on config.php
