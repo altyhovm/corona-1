3f759511ba72a0cbfd74bcd0dfab999d33604ba9
Tugrul Bıyık <44803788+tugib@users.noreply.github.com>
2020-04-04 04:00:12 +0300

    update

--- config.php
+++ config.php
@@ -2,6 +2,14 @@
 $url = "http://localhost/corona/";
 $json=file_get_contents("http://tbiyik.com/covid/api");
 $values =  json_decode($json);
+usort($values->data, function($a, $b) {
+    if ($a->dead == $b->dead) {
+        return 0;
+    }
+    return $a->dead > $b->dead ? -1 : 1;
+});
+
+
 function replace($txt)
 {
   $giren = array(" ", "'");
@@ -9,12 +17,14 @@ function replace($txt)
   $veri = str_replace($giren, $cikan, $txt);
   return $veri;
 }
+
+
 ?>
 <!DOCTYPE html>
 <html lang="en" dir="ltr">
   <head>
     <meta charset="utf-8">
-    <title>COVID-19 - LIVE</title>
+    <title> <?php if(isset($_GET["location"])){echo $_GET["location"]; } ?>  - COVID-19 LIVE</title>
     <link href="https://fonts.googleapis.com/css2?family=Roboto&display=swap" rel="stylesheet">
     <link rel="stylesheet" href="<?=$url?>style.css">
   </head>
